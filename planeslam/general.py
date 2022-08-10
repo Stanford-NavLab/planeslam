ce3fc3c0391ebcdfd2fda3a5eb4cc9116b05946a
Adam Dai <adamdai97@gmail.com>
2022-08-10 14:20:37 -0700

    trajectory pose plotting

--- planeslam/general.py
+++ planeslam/general.py
@@ -144,14 +144,73 @@ def color_legend(ax, num_colors):
 def pc_plot_trace(P):
     """Generate plotly plot trace for point cloud
 
+    Parameters
+    ----------
+    P : np.array (N x 3)
+        Point cloud
+
     Returns
     -------
-    fig : plotly go.Scatter3d
+    go.Scatter3d
         Scatter plot trace
 
     """
     return go.Scatter3d(x=P[:,0], y=P[:,1], z=P[:,2], 
         mode='markers', marker=dict(size=2))
+
+
+def pose_plot_trace(R, t):
+    """Generate plotly plot trace for a 3D pose (position and orientation)
+
+    Parameters
+    ----------
+    R : np.array (3 x 3)
+        Rotation matrix representing orientation
+    t : np.array (3)
+        Translation vector representing position
+    
+    Returns
+    -------
+    list
+        List containing traces for plotting 3D pose
+
+    """
+    point = go.Scatter3d(x=[t[0]], y=[t[1]], z=[t[2]], 
+                mode='markers', marker=dict(size=5))
+    xs = []; ys = []; zs = []
+    for i in range(3):
+        xs += [t[0], t[0] + R[0,i], None]
+        ys += [t[1], t[1] + R[1,i], None]
+        zs += [t[2], t[2] + R[2,i], None]
+    lines = go.Scatter3d(x=xs, y=ys, z=zs, mode="lines", showlegend=False)
+    return [point, lines]
+
+
+def trajectory_plot_trace(Rs, ts, color="red"):
+    """Generate plotly plot trace for a 3D trajectory of poses
+
+    Parameters
+    ----------
+    Rs : np.array (3 x 3 x N)
+        Sequence of orientations
+    ts : np.array (N x 3)
+        Sequence of positions
+    
+    Returns
+    -------
+    list
+        List containing traces for plotting 3D trajectory
+    
+    """
+    points = go.Scatter3d(x=[ts[:,0]], y=[ts[:,1]], z=[ts[:,2]], showlegend=False)#, mode='markers', marker=dict(size=5))
+    xs = []; ys = []; zs = []
+    for i in range(len(ts)):
+        for j in range(3):
+            xs += [ts[i,0], ts[i,0] + Rs[0,j,i], None]
+            ys += [ts[i,1], ts[i,1] + Rs[1,j,i], None]
+            zs += [ts[i,2], ts[i,2] + Rs[2,j,i], None]
+    lines = go.Scatter3d(x=xs, y=ys, z=zs, mode="lines", line=dict(color=color), showlegend=False)
+    return [points, lines]
     
 
 def plot_normals(P,normals,scale=10.):
